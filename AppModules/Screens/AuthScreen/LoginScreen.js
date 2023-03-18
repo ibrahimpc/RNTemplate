8733524533e62a67a9a7a5ab1ddd876932682b5b
ibrahim-foodhub <mohammedibrahim.f@foodhub.com>
2023-03-18 20:36:09 +0530

    code cleanup

--- AppModules/Screens/AuthScreen/LoginScreen.js
+++ AppModules/Screens/AuthScreen/LoginScreen.js
@@ -33,17 +33,20 @@ const LoginScreen = ({navigation}) => {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
       if (userInfo) dispatch(isUserLoggedIn(true));
-      console.log(userInfo, '111');
     } catch (error) {
       setDisableGoogleSignIn(false);
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
+        console.warn('Login Cancelled')
       } else if (error.code === statusCodes.IN_PROGRESS) {
         // operation (e.g. sign in) is in progress already
+        console.warn('Signing in progress')
       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
         // play services not available or outdated
+        console.warn('Play Service not available')
       } else {
         // some other error happened
+        console.warn('Oops something went wrong')
       }
     }
   };
@@ -66,9 +69,6 @@ const LoginScreen = ({navigation}) => {
           style={authStyles.submitButton}>
           <Text style={authStyles.submitButtonText}>Submit</Text>
         </TouchableOpacity>
-        {/*<TouchableOpacity onPress={signIn}>*/}
-        {/*  <Text>google sign in</Text>*/}
-        {/*</TouchableOpacity>*/}
         <Text>or</Text>
         <GoogleSigninButton
           style={{width: 192, height: 48}}
@@ -77,86 +77,8 @@ const LoginScreen = ({navigation}) => {
           onPress={signIn}
           disabled={disableGoogleSignIn}
         />
-        {/*<TextInput*/}
-        {/*    value={phoneNo}*/}
-        {/*    onChangeText={text => setPhoneNo(text)}*/}
-        {/*    style={{width: 250, height: 40, borderWidth: 1, padding: 10}}*/}
-        {/*    placeholder={'Phone No'}*/}
-        {/*/>*/}
-        {/*<TouchableOpacity*/}
-        {/*    style={{*/}
-        {/*      height: 40,*/}
-        {/*      backgroundColor: 'blue',*/}
-        {/*      alignItems: 'center',*/}
-        {/*      justifyContent: 'center',*/}
-        {/*      marginTop: 5,*/}
-        {/*      borderRadius: 5,*/}
-        {/*    }}*/}
-        {/*    onPress={signInWithPhoneNumber}>*/}
-        {/*  <Text style={{color: 'white', fontSize:10}}>Get OTP</Text>*/}
-        {/*</TouchableOpacity>*/}
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
 export default LoginScreen;
-
-//
-//
-// import React, {useEffect, useState} from 'react';
-// import { Button, TextInput } from 'react-native';
-// import auth from '@react-native-firebase/auth';
-//
-// function LoginScreen() {
-//   // If null, no SMS has been sent
-//   const [confirm, setConfirm] = useState(null);
-//
-//   // verification code (OTP - One-Time-Passcode)
-//   const [code, setCode] = useState('');
-//
-//   // Handle login
-//   function onAuthStateChanged(user) {
-//     if (user) {
-//       // Some Android devices can automatically process the verification code (OTP) message, and the user would NOT need to enter the code.
-//       // Actually, if he/she tries to enter it, he/she will get an error message because the code was already used in the background.
-//       // In this function, make sure you hide the component(s) for entering the code and/or navigate away from this screen.
-//       // It is also recommended to display a message to the user informing him/her that he/she has successfully logged in.
-//     }
-//   }
-//
-//   useEffect(() => {
-//     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-//     return subscriber; // unsubscribe on unmount
-//   }, []);
-//
-//   // Handle the button press
-//   async function signInWithPhoneNumber(phoneNumber) {
-//     const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
-//     setConfirm(confirmation);
-//   }
-//
-//   async function confirmCode() {
-//     try {
-//       await confirm.confirm(code);
-//     } catch (error) {
-//       console.log('Invalid code.');
-//     }
-//   }
-//
-//   if (!confirm) {
-//     return (
-//         <Button
-//             title="Phone Number Sign In"
-//             onPress={() => signInWithPhoneNumber('+91 7338942977')}
-//         />
-//     );
-//   }
-//
-//   return (
-//       <>
-//         <TextInput value={code} onChangeText={text => setCode(text)} />
-//         <Button title="Confirm Code" onPress={() => confirmCode()} />
-//       </>
-//   );
-// }
-// export default LoginScreen;
